2b8b420e6cbd8aaf5a365e519e6b9f9a83c6f62e
denisbao <denis.tpd@gmail.com>
2022-02-03 15:41:44 -0300

    Adicionando e configurando MirageJS nas Categorias

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+
+// Configuração do MirageJS:
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
