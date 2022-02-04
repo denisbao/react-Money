1f6de2cb6aaeedff33a57b106f9cb2524799ba03
denisbao <denis.tpd@gmail.com>
2022-02-04 09:19:47 -0300

    Estilizando botões das Transações (Polished)

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-import closeImg from '../../assets/close.svg'
-import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean,
@@ -21,6 +23,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       
       <Container>
         <h2>Cadastrar Transação</h2>
+        
         <input
           placeholder='Título'
         />
@@ -29,6 +32,18 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
           type="number"
           placeholder='Valor' 
         />
+
+        <TransactionTypeContainer>
+          <button type='button'>
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type='button'>
+            <img src={outcomeImg} alt="Saída" />
+            <span>Entrada</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input 
           placeholder='Categoria'
