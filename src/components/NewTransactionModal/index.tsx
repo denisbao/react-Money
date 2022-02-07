b352ac15471f6d6d7c1c8d69e5054d910ee557ad
denisbao <denis.tpd@gmail.com>
2022-02-07 10:14:26 -0300

    Movendo crianção de transações dentro do contexto

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,12 @@
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
+import { TransactionsContext } from '../../TransactionsContext';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
+
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { Container, TransactionTypeContainer, RadioBox } from './styles';
+
 
 interface NewTransactionModalProps {
   isOpen: boolean,
@@ -13,27 +15,23 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
 
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [type, setType] = useState('deposit');
   const [category, setCategory] = useState('');
-
-
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+    createTransaction({
       title,
-      value,
+      amount,
       category,
-      type
-    }
-
-    api.post('/transactions', data)
+      type,
+    })
   }
-
-
 
   return (
     <Modal 
@@ -58,8 +56,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <input 
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
