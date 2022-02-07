9f4521ef7a95edfea78682f1c2e53a44f2658096
denisbao <denis.tpd@gmail.com>
2022-02-07 11:14:12 -0300

    Transformando contexto de transação em um hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,12 +3,11 @@ import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
 
 import { Container } from './styles'
-import { useContext } from 'react'
-import { TransactionsContext } from '../../TransactionsContext'
+import { useTransactions } from '../../hooks/useTransactions'
 
 export function Summary() {
 
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
   
   // const totalDeposits = transactions.reduce((acc, transaction) => {
   //   if (transaction.type === 'deposit') {
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import {createContext, ReactNode, useEffect, useState} from 'react';
-import { api } from './services/api';
+import {createContext, ReactNode, useContext, useEffect, useState} from 'react';
+import { api } from '../services/api';
 
 
 interface Transaction {
@@ -32,7 +32,7 @@ interface TransactionsContextData {
 }
 
 
-export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export function TransactionsProvider({children}: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -61,4 +61,9 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+  return context;
 }
